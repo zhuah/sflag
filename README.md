6a3021e6e8c628b953c54c47de1899bda9406f57
aihui zhu <mr.zhuah@gmail.com>
2021-03-15 16:17:10 +0800

    improve non-flag field and sub-command, support custom command name resolver.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 sflag is a small/simple/structure enhancement of stdlib `flag`
 
 # Features
-* support subcommand
+* support subcommand with global options
 * define/parse flags with structure
-* catch non-flag values into structure field tagged with `name:"#nonflag"`
+* catch non-flag values into structure field tagged with `name:"#"` or `name:"#CUSTOM_DISPLAY_NAME"`
 
 # Usage
 structure tags:
 * name: flag name without dash prefix, separate multiple names by comma
 * usage: flag usage/description
+* env: get value from environment variable
 * default: flag default value
 
-catch non-flag values with `name:"#nonflag"`
+catch non-flag values with `name:"#"` or `name:"#CUSTOM_DISPLAY_NAME"`
 * allows multiple non-flag field, catch by order of appearance if have more than one.
 * support both `string/[]string` to catch one or more values, but can have only one field with type `[]string`
 * non-flag value will not allowed if there are no non-flag fields.
--- flag.go
+++ flag.go
@@ -15,61 +15,26 @@ import (
 	"unsafe"
 )
 
+var ErrHelp = flag.ErrHelp
+
 type UsageFunc func(printDefaults func(w io.Writer))
+
+type CommandResolveFunc func(args []string, commands []Command) ([]string, bool)
 
 type Command struct {
 	Name  string
 	Usage string
 
+	// global flags must not be nil with this option
+	RunWithFlags func(flags interface{}, args []string)
+	// global flags will be ignored even if not nil
 	Run func(args []string)
-}
-
-type CommandList struct {
-	Commands []Command
-
-	Usage UsageFunc
-}
-
-func (c CommandList) Run(args []string) {
-	var name string
-	if len(args) > 1 {
-		name = args[1]
-	}
-	if name == "" || isHelpFlag(name) {
-		c.usage()
-		return
-	}
-
-	for i := range c.Commands {
-		if c.Commands[i].Name == name {
-			c.Commands[i].Run(args[1:])
-			return
-		}
-	}
-	fprintf(os.Stderr, "unsupported command `%s`,\n", name)
-	os.Exit(1)
-}
-
-func (c CommandList) usage() {
-	if c.Usage == nil {
-		c.printDefaults(os.Stderr)
-	} else {
-		c.Usage(c.printDefaults)
-	}
-}
-
-func (c CommandList) printDefaults(w io.Writer) {
-	tw := tabWriter(w, 2)
-	fprintln(tw, "Available commands:")
-	for _, c := range c.Commands {
-		fprintf(tw, "\t%s\t%s\n", c.Name, c.Usage)
-	}
-	_ = tw.Flush()
 }
 
 type flagInfo struct {
 	Name    string
-	Default interface{}
+	Env     string
+	Default string
 	Usage   string
 	Type    string
 
@@ -82,46 +47,21 @@ type commandFlags struct {
 	flags          []flagInfo
 	stringNonFlags []flagInfo
 	sliceNonFlag   []flagInfo
+
+	subcommands []Command
+
+	usage UsageFunc
 }
 
-func printDefaultValue(value interface{}) (string, bool) {
-	if value == nil {
-		return "", false
-	}
-
-	v := reflect.ValueOf(value)
-	switch v.Kind() {
-	case reflect.Bool:
-		b := v.Bool()
-		return "true", b
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		i := v.Int()
-		if i == 0 {
-			return "", false
-		}
-		return strconv.Itoa(int(i)), true
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		i := v.Uint()
-		if i == 0 {
-			return "", false
-		}
-		return strconv.FormatUint(i, 10), true
-	case reflect.String:
-		s := v.String()
-		return strconv.Quote(s), s != ""
-	default:
-		return "", false
-	}
-}
-
-func (c commandFlags) printDefaults(w io.Writer) {
-	if len(c.flags)+len(c.stringNonFlags)+len(c.sliceNonFlag) == 0 {
+func (c *commandFlags) printDefaults(w io.Writer) {
+	if len(c.flags)+len(c.stringNonFlags)+len(c.sliceNonFlag)+len(c.subcommands) == 0 {
 		fprintln(w, "no options.")
 		return
 	}
 	tw := tabWriter(w, 2)
 
-	if len(c.stringNonFlags) > 0 || len(c.sliceNonFlag) > 0 {
+	hasFlag := len(c.flags) > 0 || len(c.stringNonFlags) > 0 || len(c.sliceNonFlag) > 0
+	if hasFlag || len(c.subcommands) > 0 {
 		fprintf(tw, "Usage: %s", c.name)
 		if len(c.flags) == 1 {
 			fprintf(tw, " [OPTION]")
@@ -134,109 +74,226 @@ func (c commandFlags) printDefaults(w io.Writer) {
 		for _, f := range c.sliceNonFlag {
 			fprintf(tw, " %s...", f.Name)
 		}
+		if len(c.subcommands) > 0 {
+			fprintf(tw, " COMMAND [ARGUMENT]...")
+		}
 		fprintf(tw, "\n")
 	} else {
 		fprintf(tw, "Usage of %s:\n", c.name)
 	}
-	for _, fs := range [][]flagInfo{c.flags, c.stringNonFlags, c.sliceNonFlag} {
-		for _, f := range fs {
-			fprintf(tw, "\t%s\t%s", f.Name, f.Type)
-			if str, ok := printDefaultValue(f.Default); ok {
-				fprintf(tw, ` (default: %s)`, str)
+	if hasFlag {
+		fprintf(tw, "\nOptions:\n")
+		for _, fs := range [][]flagInfo{c.flags, c.stringNonFlags, c.sliceNonFlag} {
+			for _, f := range fs {
+				fprintf(tw, "\t%s\t%s", f.Name, f.Type)
+				if f.Default != "" || f.Env != "" {
+					fprintf(tw, ` (`)
+					if f.Default != "" {
+						fprintf(tw, ` default: %s`, f.Default)
+					}
+					if f.Env != "" {
+						if f.Default != "" {
+							fprintf(tw, `, `)
+						}
+						fprintf(tw, ` env: %s`, f.Env)
+					}
+
+					fprintf(tw, `)`)
+				}
+				fprintln(tw)
+				if f.Usage != "" {
+					fprintf(tw, "\t\t%s\n", f.Usage)
+				}
 			}
-			fprintln(tw)
-			if f.Usage != "" {
-				fprintf(tw, "\t\t%s\n", f.Usage)
-			}
+		}
+	}
+	if len(c.subcommands) > 0 {
+		fprintf(tw, "\nCommands:\n")
+		for _, cmd := range c.subcommands {
+			fprintf(tw, "\t%s\t%s\n", cmd.Name, cmd.Usage)
 		}
 	}
 	_ = tw.Flush()
 }
 
+func (c *commandFlags) printHelp() {
+	if c.usage == nil {
+		c.printDefaults(os.Stderr)
+	} else {
+		c.usage(c.printDefaults)
+	}
+}
+
 var flagValueType = reflect.TypeOf((*flag.Value)(nil)).Elem()
 
-func addFlag(val reflect.Value, cmdline *flag.FlagSet, names []string, defstr, usage string, ptr unsafe.Pointer) (interface{}, bool) {
+type commonflagValue struct {
+	val reflect.Value
+}
+
+var _ flag.Value = &commonflagValue{}
+
+func (p *commonflagValue) IsBoolFlag() bool {
+	return p.val.Kind() == reflect.Bool
+}
+
+func (p *commonflagValue) String() string {
+	switch p.val.Kind() {
+	case reflect.Bool:
+		return strconv.FormatBool(p.val.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(p.val.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatUint(p.val.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(p.val.Float(), 'f', -1, 64)
+	case reflect.String:
+		return p.val.String()
+	}
+	panic("unreachable")
+}
+
+func (p *commonflagValue) Set(s string) error {
+	switch p.val.Kind() {
+	case reflect.Bool:
+		b, err := strconv.ParseBool(s)
+		if err != nil {
+			return err
+		}
+		p.val.SetBool(b)
+		return nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v, err := strconv.ParseInt(s, 10, int(p.val.Type().Size()*8))
+		if err != nil {
+			return err
+		}
+		p.val.SetInt(v)
+		return nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v, err := strconv.ParseUint(s, 10, int(p.val.Type().Size()*8))
+		if err != nil {
+			return err
+		}
+		p.val.SetUint(v)
+		return nil
+	case reflect.Float32, reflect.Float64:
+		v, err := strconv.ParseFloat(s, int(p.val.Type().Size()*8))
+		if err != nil {
+			return err
+		}
+		p.val.SetFloat(v)
+		return nil
+	case reflect.String:
+		p.val.SetString(s)
+		return nil
+	}
+	panic("unreachable")
+}
+
+func addFlag(val reflect.Value, cmdline *flag.FlagSet, names []string, env, defstr, usage string, ptr unsafe.Pointer) (string, bool) {
 	iterNames := func(fn func(name string)) {
 		for _, name := range names {
 			fn(name)
 		}
 	}
 
-	var (
-		defval interface{}
-		skip   bool
-	)
+	var fval flag.Value
 	switch val.Kind() {
-	case reflect.Bool:
-		def, _ := strconv.ParseBool(defstr)
-		defval = def
-		iterNames(func(name string) {
-			cmdline.BoolVar((*bool)(ptr), name, def, usage)
-		})
-	case reflect.Int:
-		def, _ := strconv.ParseInt(defstr, 10, strconv.IntSize)
-		iterNames(func(name string) {
-			cmdline.IntVar((*int)(ptr), name, int(def), usage)
-		})
-	case reflect.Int64:
-		def, _ := strconv.ParseInt(defstr, 10, 64)
-		defval = def
-		iterNames(func(name string) {
-			cmdline.Int64Var((*int64)(ptr), name, def, usage)
-		})
-	case reflect.Uint:
-		def, _ := strconv.ParseUint(defstr, 10, strconv.IntSize)
-		defval = def
-		iterNames(func(name string) {
-			cmdline.UintVar((*uint)(ptr), name, uint(def), usage)
-		})
-	case reflect.Uint64:
-		def, _ := strconv.ParseUint(defstr, 10, 64)
-		defval = def
-		iterNames(func(name string) {
-			cmdline.Uint64Var((*uint64)(ptr), name, def, usage)
-		})
-	case reflect.Float64:
-		def, _ := strconv.ParseFloat(defstr, 64)
-		defval = def
-		iterNames(func(name string) {
-			cmdline.Float64Var((*float64)(ptr), name, def, usage)
-		})
-	case reflect.String:
-		defval = defstr
-		iterNames(func(name string) {
-			cmdline.StringVar((*string)(ptr), name, defstr, usage)
-		})
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64,
+		reflect.String:
+		fval = &commonflagValue{val}
 	default:
-		defval = defstr
-		if reflect.PtrTo(val.Type()).Implements(flagValueType) {
-			val := val.Addr().Interface().(flag.Value)
-			if defstr != "" {
-				_ = val.Set(defstr)
-			}
-			iterNames(func(name string) {
-				cmdline.Var(val, name, usage)
-			})
-		} else {
-			skip = true
+		if !reflect.PtrTo(val.Type()).Implements(flagValueType) {
+			return "", false
+		}
+		fval = val.Addr().Interface().(flag.Value)
+	}
+	var valApplied bool
+	if env != "" {
+		enval := os.Getenv(env)
+		if enval != "" {
+			valApplied = fval.Set(enval) == nil
 		}
 	}
 
-	return defval, !skip
+	if defstr != "" {
+		if !valApplied {
+			valApplied = fval.Set(defstr) == nil
+		}
+	}
+	if defstr != "" && val.Kind() == reflect.String {
+		defstr = strconv.Quote(defstr)
+	}
+	iterNames(func(name string) {
+		cmdline.Var(fval, name, usage)
+	})
+
+	return defstr, true
 }
 
-func Parse(args []string, usage UsageFunc, ptr interface{}) error {
-	var err error
-	if ptr == nil {
-		if len(args) > 1 {
-			err = errors.New("the command should be runs without arguments")
+type Parser struct {
+	Usage UsageFunc
+
+	CommandResolver CommandResolveFunc
+}
+
+func (p *Parser) resolveSubCommand(commands []Command, args []string) (Command, []string, error) {
+	cmdname := args[0]
+	lookup := func(name string) (Command, bool) {
+		for _, cmd := range commands {
+			if cmd.Name == name {
+				return cmd, true
+			}
 		}
-		return nil
+		return Command{}, false
+	}
+	cmd, ok := lookup(cmdname)
+	if ok {
+		return cmd, args, nil
 	}
 
-	cmdline := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	if p.CommandResolver != nil {
+		if args, ok := p.CommandResolver(args, commands); ok {
+			cmd, ok := lookup(args[0])
+			if ok {
+				return cmd, args, nil
+			}
+		}
+	}
 
-	refv := reflect.ValueOf(ptr)
+	return Command{}, nil, fmt.Errorf("unknown command: %s", cmdname)
+}
+
+func (p *Parser) parse(args []string, flagsPtr interface{}, commands []Command) (subcmd Command, subcommand []string, err error) {
+	flags := commandFlags{
+		name:        args[0],
+		subcommands: commands,
+		usage:       p.Usage,
+	}
+	cmdline := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	cmdline.Usage = flags.printHelp
+	if flagsPtr == nil {
+		// check for help flag
+		err := cmdline.Parse(args[1:])
+		if err != nil {
+			return subcmd, nil, err
+		}
+		nonFlagArgs := cmdline.Args()
+		if len(commands) > 0 {
+			if len(nonFlagArgs) == 0 {
+				return subcmd, nil, fmt.Errorf("no command to be run")
+			}
+			return p.resolveSubCommand(commands, nonFlagArgs)
+		}
+		if len(nonFlagArgs) > 0 {
+			return subcmd, nil, errors.New("the command should be runs without arguments")
+		}
+		return subcmd, nil, nil
+	}
+
+	refv := reflect.ValueOf(flagsPtr)
 	if refv.Kind() != reflect.Ptr {
 		panic("expect pointer of struct")
 	}
@@ -250,10 +307,6 @@ func Parse(args []string, usage UsageFunc, ptr interface{}) error {
 	var (
 		nonFlagStringFields []reflect.Value
 		nonFlagSliceField   reflect.Value
-
-		flags = commandFlags{
-			name: args[0],
-		}
 	)
 
 	for i := 0; i < numField; i++ {
@@ -266,32 +319,37 @@ func Parse(args []string, usage UsageFunc, ptr interface{}) error {
 
 		name := ftyp.Tag.Get("name")
 		usage := ftyp.Tag.Get("usage")
+		env := ftyp.Tag.Get("env")
 		if name == "-" {
 			continue
 		}
-		if name == "#nonflag" {
+		if strings.HasPrefix(name, "#") {
+			name := strings.TrimPrefix(name, "#")
+			if name == "" {
+				name = strings.ToUpper(ftyp.Name)
+			}
 			switch {
 			case ftyp.Type.Kind() == reflect.String:
 				nonFlagStringFields = append(nonFlagStringFields, fval)
 				flags.stringNonFlags = append(flags.stringNonFlags, flagInfo{
-					Name:    strings.ToUpper(ftyp.Name),
+					Name:    name,
 					Usage:   usage,
 					Type:    "string",
 					NonFlag: true,
 				})
 			case ftyp.Type == reflect.TypeOf((*[]string)(nil)).Elem():
 				if nonFlagSliceField.IsValid() {
-					panic(fmt.Errorf("duplicated `#nonflag` field of type []string: %s", ftyp.Name))
+					panic(fmt.Errorf("duplicated non-flag field of type []string: %s", ftyp.Name))
 				}
 				nonFlagSliceField = fval
 				flags.sliceNonFlag = append(flags.sliceNonFlag, flagInfo{
-					Name:         strings.ToUpper(ftyp.Name),
+					Name:         name,
 					Usage:        usage,
 					Type:         "string",
 					NonFlagSlice: true,
 				})
 			default:
-				panic(fmt.Errorf("only string/[]string allowed for `#nonflag` field: %s", ftyp.Name))
+				panic(fmt.Errorf("only string/[]string allowed for non-flag field: %s", ftyp.Name))
 			}
 
 			continue
@@ -316,7 +374,7 @@ func Parse(args []string, usage UsageFunc, ptr interface{}) error {
 
 		names := splitAndTrim(name)
 		defstr := ftyp.Tag.Get("default")
-		defval, ok := addFlag(fval, cmdline, names, defstr, usage, ptr)
+		defstr, ok := addFlag(fval, cmdline, names, env, defstr, usage, ptr)
 		if !ok {
 			continue
 		}
@@ -328,57 +386,122 @@ func Parse(args []string, usage UsageFunc, ptr interface{}) error {
 			Name:    strings.Join(names, "/"),
 			Usage:   usage,
 			Type:    ftyp.Type.Kind().String(),
-			Default: defval,
+			Env:     env,
+			Default: defstr,
 			NonFlag: true,
 		})
 	}
-
-	cmdline.Usage = func() {
-		if usage == nil {
-			flags.printDefaults(os.Stderr)
-		} else {
-			usage(flags.printDefaults)
-		}
+	if nonFlagSliceField.IsValid() && len(commands) > 0 {
+		panic(fmt.Errorf("non-flag field of type []string is not allowed with sub commands: %s", flags.sliceNonFlag[0].Name))
 	}
+
 	err = cmdline.Parse(args[1:])
 	if err != nil {
-		return err
+		return subcmd, nil, err
 	}
 
 	nonflagArgs := cmdline.Args()
+	var consumedNonFlagArgs int
 	for i, s := range nonflagArgs {
 		if i < len(nonFlagStringFields) {
 			nonFlagStringFields[i].SetString(s)
+			consumedNonFlagArgs = i + 1
 		} else if nonFlagSliceField.IsValid() {
 			nonFlagSliceField.Set(reflect.ValueOf(nonflagArgs[i:]))
+			consumedNonFlagArgs = len(nonflagArgs)
 			break
 		} else {
-			if len(nonFlagStringFields) == 0 {
-				return fmt.Errorf("non-flag args not allowed: %v", nonflagArgs)
-			}
-			return fmt.Errorf("accept only %d non-flag args: %v", len(nonFlagStringFields), nonflagArgs)
+			break
 		}
 	}
-	return nil
+	if consumedNonFlagArgs < len(nonflagArgs) {
+		if len(commands) == 0 {
+			if len(nonFlagStringFields) == 0 {
+				return subcmd, nil, fmt.Errorf("non-flag args not allowed: %v", nonflagArgs)
+			}
+			return subcmd, nil, fmt.Errorf("accept only %d non-flag args: %v", len(nonFlagStringFields), nonflagArgs)
+		}
+		return p.resolveSubCommand(commands, nonflagArgs[consumedNonFlagArgs:])
+	}
+	if len(commands) > 0 {
+		return subcmd, nil, fmt.Errorf("no command to be run")
+	}
+	return subcmd, nil, nil
 }
 
-func MustParse(args []string, usage UsageFunc, flags interface{}) {
-	err := Parse(args, usage, flags)
+func (p *Parser) Parse(args []string, ptr interface{}) error {
+	_, _, err := p.parse(args, ptr, nil)
+	return err
+}
+
+func (p *Parser) MustParse(args []string, flags interface{}) {
+	err := p.Parse(args, flags)
+	handleError(err)
+}
+
+func (p *Parser) ParseCommand(args []string, globalFlags interface{}, commands ...Command) (cmd Command, cmdArgs []string, err error) {
+	if len(commands) == 0 {
+		panic("should provide at least one command.")
+	}
+	return p.parse(args, globalFlags, commands)
+}
+
+func (p *Parser) MustParseCommand(args []string, globalFlags interface{}, commands ...Command) (cmd Command, cmdArgs []string) {
+	cmd, cmdArgs, err := p.ParseCommand(args, globalFlags, commands...)
+	handleError(err)
+	return cmd, cmdArgs
+}
+
+func (p *Parser) RunCommand(args []string, globalFlags interface{}, commands ...Command) {
+	cmd, cmdArgs, err := p.ParseCommand(args, globalFlags, commands...)
 	if err != nil {
-		if errors.Is(err, flag.ErrHelp) {
+		handleError(err)
+		return
+	}
+
+	if globalFlags == nil {
+		if cmd.Run != nil {
+			cmd.Run(cmdArgs)
+		} else {
+			panic(fmt.Errorf("Command.Run is nil: %s", cmd.Name))
+		}
+	} else {
+		if cmd.Run != nil {
+			cmd.Run(cmdArgs)
+		} else if cmd.RunWithFlags != nil {
+			cmd.RunWithFlags(globalFlags, cmdArgs)
+		} else {
+			panic(fmt.Errorf("Command.Run/RunWithFlags are both nil: %s", cmd.Name))
+		}
+	}
+}
+
+func Parse(args []string, ptr interface{}) error {
+	return (&Parser{}).Parse(args, ptr)
+}
+func MustParse(args []string, ptr interface{}) {
+	(&Parser{}).MustParse(args, ptr)
+}
+func ParseCommand(args []string, globalFlagsPtr interface{}, commands ...Command) (cmd Command, cmdArgs []string, err error) {
+	return (&Parser{}).ParseCommand(args, globalFlagsPtr, commands...)
+}
+func MustParseCommand(args []string, globalFlagsPtr interface{}, commands ...Command) (cmd Command, cmdArgs []string) {
+	return (&Parser{}).MustParseCommand(args, globalFlagsPtr, commands...)
+}
+func RunCommand(args []string, globalFlagsPtr interface{}, commands ...Command) {
+	(&Parser{}).RunCommand(args, globalFlagsPtr, commands...)
+}
+func handleError(err error) {
+	if err != nil {
+		if errors.Is(err, ErrHelp) {
 			os.Exit(0)
 		} else {
 			os.Exit(1)
 		}
 	}
 }
-
 func tabWriter(out io.Writer, width int) *tabwriter.Writer {
 	return tabwriter.NewWriter(out, 0, 0, width, ' ', 0)
-}
-
-func isHelpFlag(name string) bool {
-	return name == "-h" || name == "-help" || name == "--help"
 }
 
 func isExported(name string) bool {
